60e5fe4e0209d1453049a9090dd08db5f0bd3ed0
josenberg <mbx.philipe@gmail.com>
2016-12-02 14:36:00 -0200

    prototype of the gallery

--- app/app.js
+++ app/app.js
@@ -1,17 +1,25 @@
 // That belongs to a museum
 (function() {
     var app = angular.module('museum', []);
-    
+
     /**
      * Controller that handle the load and beavior of the museum
      **/
     app.controller("MuseumController", ["$http", function($http) {
       this.paints = [];
-      var museum = this;
+      this.currentSelectedPaint = {};
 
+      var museum = this;
 
       $http.get("/assets/library.json").then(function(rs) {
         museum.paints = rs.data.paints;
-      })
+        museum.currentSelectedPaint = museum.paints[0];
+      });
+
+      this.setCurrent = function(paint) {
+        console.log("paint")
+        this.currentSelectedPaint = paint;
+      };
+
     }])
 })();
